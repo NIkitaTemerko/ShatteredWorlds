88c3e096f333cefd6adb5088b43fdf7e20d06650
Nikita Temerko <ntemerko@almasc.ru>
2025-07-01 23:39:47 +0300

    fix: releast

--- scripts/update-system-version.js
+++ scripts/update-system-version.js
@@ -20,11 +20,8 @@ sys.version = pkg.version;
 
 sys.download =
   `https://github.com/NIkitaTemerko/ShatteredWorlds/releases/download/v${pkg.version}/shattered-worlds.zip`;
-sys.manifest =
-  `https://raw.githubusercontent.com/NIkitaTemerko/ShatteredWorlds/${pkg.version}/system.json`;
 // записываем обратно, сохраняя отступы
 writeFileSync(sysPath, JSON.stringify(sys, null, 2) + "\n", "utf8");
 
 console.log(`✅ system.json version updated to ${sys.version}`);
 console.log(`⬇️ system.json download updated to ${sys.download}`);
-console.log(`🧾 system.json manifest updated to ${sys.manifest}`);

d8fe43e5d74a9e6282568ab12ca75db758fb8d04
Nikita Temerko <ntemerko@almasc.ru>
2025-11-13 21:12:47 +0300

    refactor: remove unused defaultOptions from CharacterApp and clean up main.css

--- src/view/BaseCharacter/CharacterApp.ts
+++ src/view/BaseCharacter/CharacterApp.ts
@@ -4,11 +4,6 @@ import CharacterShell from './ui/RootCharacterShell.svelte';
 export class CharacterApp extends SvelteActorSheet {
   /* обязательное статическое свойство, чтобы оболочка знала, какой Shell монтировать */
   static Shell = CharacterShell;
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(SvelteActorSheet.defaultOptions, {
-      classes: ['shw', 'sheet', 'character'],
-    });
-  }
 
   get template() {
     return 'systems/shattered-worlds/templates/actor/ShwCharacterSheet.hbs';
